5001c3eb61f8b0cf5830bf4ebe921f3543bd1637
Bhaskar(50082728) <bhaskarnarayan@ltfs.com>
2025-07-24 13:16:54 +0530

    app module updated

--- lib/config/app_module.dart
+++ lib/config/app_module.dart
@@ -1,15 +1,28 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:navigation_config/navigation/module_navigation_delegate.dart';
 import '../config/app_config_model.dart';
 
-abstract class AppModule {
+abstract class AppModule extends StatefulWidget {
+  AppModule({super.key});
+
   String get id;
 
-  Widget build(
-    BuildContext context,
-    AppConfig config,
-    Dio client, {
-    required ModuleNavigationCallback onNext,
-  });
+  late AppConfig config;
+  late Dio client;
+  late ModuleNavigationCallback onNext;
+
+  Widget build(BuildContext context);
+
+  @override
+  State<AppModule> createState() => _AppModuleState();
+}
+
+class _AppModuleState extends State<AppModule> {
+  @override
+  Widget build(BuildContext context) {
+    return widget.build(context);
+  }
 }
